ed527be238a3daaab0bdd260980a056d5089cb4f
oscarfiscal <oscarfiscalcp@gmail.com>
2022-02-06 20:27:41 -0500

    configurando vue con vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
+import Vuetify from 'vuetify'
 
+import 'vuetify/dist/vuetify.min.css'
+
+
+Vue.use(Vuetify)
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+export default new Vuetify({
+  icons: {
+    iconfont: "mdiSvg,   ,mdiSvg, md, fa, fa4, faSvg",
+  },
+});
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
+<!-- @format -->
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn depressed color="error"> Error </v-btn>
   </div>
 </template>
